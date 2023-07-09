cdb7afe9e5b99f721741d2fc44653ad17691cb50
RealCryZed <bujhmgtnhjd@bk.ru>
2023-07-09 23:20:38 +0600

    added ability to reach publications page without specifying pagesize requestparam

--- src/main/java/ru/omgtu/scienceomgtu/Controller/PublicationController.java
+++ src/main/java/ru/omgtu/scienceomgtu/Controller/PublicationController.java
@@ -6,16 +6,16 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import ru.omgtu.scienceomgtu.model.Publication;
 import ru.omgtu.scienceomgtu.service.FilterService;
 import ru.omgtu.scienceomgtu.service.PublicationService;
 
-import java.util.List;
-
 @Controller
 public class PublicationController {
+
+    private final int DEFAULT_PAGE_SIZE = 10;
 
     @Autowired
     private PublicationService publicationService;
@@ -25,7 +25,8 @@ public class PublicationController {
 
     @GetMapping("/publications/page/{offset}")
     public ModelAndView getAllPublicationsFiltered(ModelAndView modelAndView, @PathVariable int offset,
-                                                   @Param("pageSize") int pageSize) {
+                                                   @RequestParam(required = false) Integer pageSize) {
+        if (pageSize == null) pageSize = DEFAULT_PAGE_SIZE;
         Page<Publication> allPublications = publicationService.findPublicationsWithPagination(offset, pageSize);
 
         modelAndView.addObject("currentPage", offset);
@@ -40,17 +41,17 @@ public class PublicationController {
         return modelAndView;
     }
 
-    @GetMapping("/publications")
-    public ModelAndView getAllPublications(ModelAndView modelAndView) {
-
-        modelAndView.addObject("ratings", filterService.getRatings());
-        modelAndView.addObject("departments", filterService.getDepartments());
-        modelAndView.addObject("publicationTypes", filterService.getPublicationTypes());
-
-        modelAndView.addObject("publications", publicationService.getTop20Publications());
-        modelAndView.setViewName("publications");
-        return modelAndView;
-    }
+//    @GetMapping("/publications")
+//    public ModelAndView getAllPublications(ModelAndView modelAndView) {
+//
+//        modelAndView.addObject("ratings", filterService.getRatings());
+//        modelAndView.addObject("departments", filterService.getDepartments());
+//        modelAndView.addObject("publicationTypes", filterService.getPublicationTypes());
+//
+//        modelAndView.addObject("publications", publicationService.getTop20Publications());
+//        modelAndView.setViewName("publications");
+//        return modelAndView;
+//    }
 
     @GetMapping("/publication/{id}")
     public ModelAndView getPublication(ModelAndView modelAndView, @PathVariable Integer id) {
