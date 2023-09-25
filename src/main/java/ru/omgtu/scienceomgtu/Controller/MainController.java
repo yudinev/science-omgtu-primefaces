e9e0e1b8961a5e1ca1779a309db3da5735ec64cf
eugene eudene <yudinev@gmail.com>
2023-09-25 09:39:02 +0600

    update

--- src/main/java/ru/omgtu/scienceomgtu/Controller/MainController.java
+++ src/main/java/ru/omgtu/scienceomgtu/Controller/MainController.java
@@ -1,7 +1,7 @@
 package ru.omgtu.scienceomgtu.Controller;
 
 
-import com.google.gson.Gson;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
